ef06e0808a7ac5cb82686f39c823b7f5476bd18f
Sebastian <sebastian.gedda@gmail.com>
2016-06-11 18:37:31 +0200

    test

---
+++ routes/index.js
@@ -0,0 +1,27 @@
+exports.nasdaq = function(req, res){
+    res.render('nasdaq');
+};
+exports.omsx = function(req,res){
+    res.render('omsx')
+}
+exports.dax = function(req,res){
+    res.render('dax')
+}
+exports.shanghai = function(req,res){
+    res.render('shanghai')
+}
+exports.hangseng = function(req,res){
+    res.render('hangseng')
+}
+exports.index = function(req, res){
+    res.render('index');
+};
+exports.gold = function(req, res){
+    res.render('gold');
+};
+exports.bitcoin = function(req, res){
+    res.render('bitcoin');
+};
+exports.oil = function(req, res){
+    res.render('oil');
+};
